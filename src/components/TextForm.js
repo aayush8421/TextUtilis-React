52b47e18061d348b954aaaa1b272dcbf287ef0a5
Aayush Chatterjee <99548784+aayush8421@users.noreply.github.com>
2023-02-03 01:35:00 +0530

    till video 8 added more logic

--- src/components/TextForm.js
+++ src/components/TextForm.js
@@ -7,6 +7,11 @@ export default function TextForm(props) {
         setText(newText)
     }
 
+    const handleLoClick = ()=>{
+        let newText = text.toLowerCase()
+        setText(newText)
+    }
+
     const handleonChange = (event)=>{
         // console.log("On Change")
         setText(event.target.value)
@@ -17,14 +22,25 @@ export default function TextForm(props) {
     // setText("new text")// correct way to change the state
 
   return (
-    <div>
+    <>
+    <div className="container">
         <h1>{props.heading}</h1>
         <div className="mb-3">
         <textarea className="form-control" id="myBox" rows="8" value={text}     
          onChange={handleonChange}></textarea>
         </div>
-        <button className="btn btn-primary" onClick={handleUpClick}>Convert to 
+        <button className="btn btn-primary mx-2" onClick={handleUpClick}>Convert to 
          UpperCase</button>
+        <button className="btn btn-primary mx-2" onClick={handleLoClick}>Convert to 
+         LowerCase</button>
     </div>
+    <div className="container my-3">
+      <h1>Your text summary</h1>
+      <p>{text === ""?0:text.split(" ").length} words and {text.length} characters</p>
+      <p>{0.008 * (text === ""?0:text.split(" ").length)} Minutes read</p>
+      <h2>Preview</h2>
+      <p>{text}</p>
+    </div>
+    </>
   )
 }
